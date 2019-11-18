1f178f90ddce6201700266e4841827d45ae35474
e <2451322819@qq.com>
2019-11-18 16:29:10 +0800

    字符串判断

---
+++ hm_10_字符串常用操作方法之判断.py
@@ -0,0 +1,15 @@
+mystr = "hello world and itcast and itheima and Python"
+
+# 1.startswith(): 判断字符串是否以某个子串开头
+# print(mystr.startswith('hello'))
+# print(mystr.startswith('hel'))
+# print(mystr.startswith('hels'))
+
+# 2.endswith()：判断字符串是否以某个子串结束
+# print(mystr.endswith('Python'))
+# print(mystr.endswith('Pythons'))
+
+
+
+
+
